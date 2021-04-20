78e0e136807124d013cf74462424b98e19acb90a
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2021-04-20 20:03:27 +0900

    probs error fatched

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,12 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <Subject title="WEB" sub="world wide web!" />
+        <TOC />
+        <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
     );
   }
